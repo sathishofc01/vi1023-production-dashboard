8d40ce62f76f5ddc19edc9d13b6a3d21dbb3ee8d
sathishofc01 <sathishofc01@gmail.com>
2026-03-26 19:25:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ else{
 alert("Wrong Password");
 }
 }
+function calculateProduction() {
+
+let morningTarget = Number(document.getElementById("morningTarget").value) || 0;
+let morningActual = Number(document.getElementById("morningActual").value) || 0;
+let morningDowntime = Number(document.getElementById("morningDowntime").value) || 0;
+
+let afternoonTarget = Number(document.getElementById("afternoonTarget").value) || 0;
+let afternoonActual = Number(document.getElementById("afternoonActual").value) || 0;
+let afternoonDowntime = Number(document.getElementById("afternoonDowntime").value) || 0;
+
+let nightTarget = Number(document.getElementById("nightTarget").value) || 0;
+let nightActual = Number(document.getElementById("nightActual").value) || 0;
+let nightDowntime = Number(document.getElementById("nightDowntime").value) || 0;
+
+
+let totalTarget = morningTarget + afternoonTarget + nightTarget;
+let totalActual = morningActual + afternoonActual + nightActual;
+let totalDowntime = morningDowntime + afternoonDowntime + nightDowntime;
+
+document.getElementById("totalTarget").innerText = totalTarget;
+document.getElementById("totalActual").innerText = totalActual;
+document.getElementById("totalDowntime").innerText = totalDowntime;
+
+}
